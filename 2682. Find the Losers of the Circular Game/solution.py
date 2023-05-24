ab40d9da8001ef08a664e88bc7976e4171bc0149
Cody <86871915+cody-berry@users.noreply.github.com>
2023-05-24 12:31:25 -0700

    Update solution.py

--- 2682. Find the Losers of the Circular Game/solution.py
+++ 2682. Find the Losers of the Circular Game/solution.py
@@ -1,21 +1,24 @@
 class Solution:
-    # taken the number of players, n, and a constant, k, it returns the losers of a circular game. player 1 starts with the ball,
-    # and passes it to the person k times away from him. The same thing for 2*k. Then 3*k. And so on until the player who has 
-    # received the ball 2 times wins and the players who didn't receive the ball at all lose.
+    # given the number of players, n, and a constant, k, it returns the losers of a
+    # circular game. player 1 starts with the ball, and passes it to the person k 
+    # players clockwise away from him. The same thing for 2*k. Then 3*k. And so on 
+    # until the player who has received the ball 2 times wins and the players who 
+    # didn't receive the ball at all lose.
     def circularGameLosers(self, n: int, k: int) -> List[int]:
-        # define an array of the people who will currently lose
+        # define an array of the people who have not received the ball yet
         losers = []
         for i in range(n):
             losers.append(i + 1)
-        print(losers)
         # which player holds the ball?
         ballHolder = 1
-        # how many iterations have gone by? (it determines who gets the ball after each round)
+        # how many iterations have gone by? (it determines who gets the ball after 
+        # each round)
         iterations = 0
         # simulate the game
         while True:
-            # remove the current ballholder from the losers list. if the ballholder has already received the ball once, or is 
-            # not a potential loser, we return all the losers. 
+            # remove the current ballholder from the losers list. if the ballholder 
+            # has already received the ball once (meaning they are not a potential 
+            # loser), we return all the losers. 
             ballHolderInLosers = False
             for loser in losers:
                 if loser == ballHolder:
@@ -24,6 +27,11 @@ class Solution:
                     break
             if not ballHolderInLosers:
                 return losers
-            # transfer the ball to another player. 
-            ballHolder = ((ballHolder + iterations*k + k - 1) % n) + 1
+            # transfer the ball to another player. the player is always based on the
+            # ballHolder, then it goes 1*k, then 2*k, then 3*k more. note that 
+            # iterations starts at 0 so we have an extra k, then since my index for 
+            # the friends are 1-indexed, I need to subtract one to make it suitable
+            # for modulo, then since it goes from 0 to n-1, we want to do 1 to n, 
+            # meaning adding. 
+            ballHolder = ((ballHolder + (iterations * k) + (k - 1)) % n) + 1
             iterations = iterations + 1
