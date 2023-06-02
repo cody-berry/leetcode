007d5b62de2c315331a23b4ec01b2fb438bbce71
Cody <86871915+cody-berry@users.noreply.github.com>
2023-06-02 11:43:10 -0700

    Submitting solution

--- 2696. Minimum String Length After Removing Substrings/solution.py
+++ 2696. Minimum String Length After Removing Substrings/solution.py
@@ -2,23 +2,27 @@ class Solution:
     # Given a string of uppercase letters, this function removes AB and CD strings as much
     # as possible and then returns the length of the string. note that removing an AB or
     # CD string might produce a new one.
+    # TODO: Replace this with multiple calls of replace()
     def minLength(self, s: str) -> int:
         # this is the index of the first character we are considering
+        index = 0
 
         # iterate until the index is 2 characters from the length of the string
-
+        while index < len(s) - 1:
             # if the pair of characters is AB or CD...
-
-                # remove the characters...
+            if s[index:(index + 2)] in ["AB", "CD"]:
+                # remove the characters... (assumes that this is the first instance)
+                s = s.replace(s[index:(index + 2)], "", 1)
 
                 # and if this is not the first index...
-
+                if (index > 0):
                     # decrement the index so that next iteration, we consider whether
                     # it cleared the way for an AB or CD. 
+                    index -= 1
 
             # otherwise...
+            else:
+                # increment index.
+                index += 1
 
-                # increment the index
-        
-        # return the length of the string
         return len(s)
