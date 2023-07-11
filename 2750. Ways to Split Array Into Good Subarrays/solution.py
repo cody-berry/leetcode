81bbf01d7b9bcbf7c8d2d9802d0fc764aee27f42
Cody <86871915+cody-berry@users.noreply.github.com>
2023-07-11 08:27:10 -0700

    Submitting solution

--- 2750. Ways to Split Array Into Good Subarrays/solution.py
+++ 2750. Ways to Split Array Into Good Subarrays/solution.py
@@ -1,11 +1,26 @@
 class Solution:
     def numberOfGoodSubarraySplits(self, nums: List[int]) -> int:
         # Iterate until the first 1. 
+        i = 0
+        try:
+            while nums[i] == 0:
+                i += 1
+        except:
+            return 0
         
         # Keep count of the number of 0s.
+        numZeroes = 0
         
         # For each number...
-        
+        result = 1
+        for j in range(i+1, len(nums)):
             # If it's a 0, add to the count of 0s.
+            if nums[j] == 0:
+                numZeroes += 1
             
             # If it's a 1, then multiply the result by the num of 0s plus 1. Then reset the count of 0s.
+            else:
+                result *= numZeroes + 1
+                numZeroes = 0
+                
+        return result % (10**9 + 7)
