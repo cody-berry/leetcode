da4ad547804516316ce300088de894259fed3a6a
Cody <86871915+cody-berry@users.noreply.github.com>
2023-08-06 09:53:37 -0700

    Submitting solution

--- 46. Permutations/solution.py
+++ 46. Permutations/solution.py
@@ -3,15 +3,25 @@ class Solution:
     # of that array.
     def permute(self, nums: List[int]) -> List[List[int]]:
         # if nums is length 1, return [[nums[0]]]
+        if len(nums) == 1:
+            return [[nums[0]]]
 
         # define the result.
+        result = []
 
         # for each number in nums...
-
+        for i in range(len(nums)):
             # permute nums again, but this time without the
             # number we're starting with.
+            newNums = nums.copy()
+            num = newNums.pop(i)
+            previousResult = self.permute(newNums)
 
             # for each element in it, append the starting num
             # to it, then append it to result.
+            for permutation in previousResult:
+                permutation.append(nums[i])
+                result.append(permutation)
 
         # return the result.
+        return result
