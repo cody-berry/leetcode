e5a06c3d885612de5928f932872c9c9bf6e536b2
Cody <86871915+cody-berry@users.noreply.github.com>
2023-08-06 09:42:25 -0700

    Submitting solution

--- 77. Combinations/solution.py
+++ 77. Combinations/solution.py
@@ -2,9 +2,7 @@ class Solution:
     # Given two integers n and k, this function returns all possible 
     # combinations of k numbers chosen from the range [1, n].
     def combine(self, n: int, k: int) -> List[List[int]]:
-        # if k is 0, return an empty list.
-
-        # otherwise, return the result of a helper function.
+        # return the result of a helper function.
         return self.rangedCombine(1, n, k)
 
     # Given three integers start, end, and k, this function returns
@@ -12,15 +10,23 @@ class Solution:
     # [start, end].
     def rangedCombine(self, start: int, end: int, k: int):
         # if k is 0, return a list containing an empty list.
+        if k == 0:
+            return [[]]
 
         # define the result.
+        result = []
 
-        # define the result of a call of rangedCombine with 1 
-        # start greater and 1 k less. 
-
-        # for each number from start to end - k inclusive...
+        # for each number from start to end - k + 1 inclusive...
+        for i in range(start, end - k + 2):
+            # define the result of a call of rangedCombine with 1 
+            # start greater than i and 1 k less. 
+            previousResult = self.rangedCombine(i + 1, end, k - 1)
 
             # for each element of this result, append the number
             # at the start.
+            for combination in previousResult:
+                combination.append(i)
+                result.append(combination)
 
         # return this result.
+        return result
