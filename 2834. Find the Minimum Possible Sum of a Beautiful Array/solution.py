38a1de03c1dc6fc969110a11d847af4e91dd54eb
Cody <86871915+cody-berry@users.noreply.github.com>
2023-09-01 12:36:36 -0700

    Submitting pseudocode

---
+++ 2834. Find the Minimum Possible Sum of a Beautiful Array/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minimumPossibleSum(self, n: int, target: int) -> int:
+        # define the current sum.
+        
+        # define the last number. starts at 1.
+        
+        # define the invalid numbers set. 
+        
+        # for n repetitions...
+        
+            # while the last number is in the invalid numbers set...
+            
+                # increment the last number. 
+                
+            # add the last number to the list (or to the current sum.)
+            
+            # add the last number and target minus the last number to 
+            # the invalid numbers set.
+            
+        # return the current sum.
